8030a9d0404d3461d419afae087b30bbbbb6d1a3
Zeonhex <eydaniriarte@gmail.com>
2024-05-17 16:05:57 -0400

    Tarea

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+<h1>Sobre nosotros</h1>
+</template>
+<script setup>
+
+</script>
---
+++ pages/index/index.vue
@@ -0,0 +1,8 @@
+<template>
+<hero></hero>
+<seccion1></seccion1>
+</template>
+<script setup>
+import Hero from "./components/hero.vue"
+import seccion1 from "./components/seccion1.vue"
+</script>
